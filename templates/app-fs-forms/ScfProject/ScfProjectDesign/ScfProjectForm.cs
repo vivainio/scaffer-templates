74926feacd0176136bdcd0137b447499bec64e88
Ville M. Vainio <ville.vainio@basware.com>
2018-07-12 09:10:16 +0300

    app-fs-forms template

---
+++ templates/app-fs-forms/ScfProject/ScfProjectDesign/ScfProjectForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TrivialBehind;
+
+namespace ScfProjectDesign
+{
+    public partial class ScfProjectForm: Form
+    {
+        public ScfProjectForm()
+        {
+            InitializeComponent();
+            // boilerplate starts
+            var disposer = TrivialBehinds.CreateBehind(this, new ScfProjectUi
+            {
+                button1 = button1,
+                label1 = label1
+            });
+            this.FormClosed += (o,e) => 
+                disposer.Dispose();
+            // boilerplate ends
+
+        }
+    }
+}
