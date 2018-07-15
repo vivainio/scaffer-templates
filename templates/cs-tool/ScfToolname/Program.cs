9e64bd124551de9a8a7841f66961732f18a10c26
Ville M. Vainio <ville.vainio@basware.com>
2018-07-15 18:09:49 +0300

    add cs-tool

---
+++ templates/cs-tool/ScfToolname/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ScfToolname
+{
+    class Program
+    {
+        public static int Main(string[] args) {
+            Console.WriteLine("Hello from scf-toolname");
+            return 0;
+
+        }
+    }
+}
---
+++ templates/cs-tool/scaffer_init.py
@@ -0,0 +1,22 @@
+import os
+
+print("hello from scaffer init")
+print(scaffer_in)
+
+print(scaffer_out)
+
+def git_data():
+    return {
+        "gitusername": os.popen("git config user.name").read().strip(),
+        "gitemail": os.popen("git config user.email").read().strip(),
+        "gitrepo": os.popen("git remote get-url origin").read().strip().rsplit(".",1)[0]
+    }
+
+d = git_data()
+scaffer_out.update(d)
+
+scaffer_out["targetdir"] = scaffer_in["target_dir"]
+scaffer_out["libname"] = os.path.basename(scaffer_in["target_dir"])
+
+
+
