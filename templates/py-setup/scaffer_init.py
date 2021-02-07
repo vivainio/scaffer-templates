1be3c4ef047ba0613545a201dd4395cae34a3e62
Ville M. Vainio <ville.vainio@basware.com>
2021-02-07 15:57:15 +0200

    fix py-setup scaffer_init

--- templates/py-setup/scaffer_init.py
+++ templates/py-setup/scaffer_init.py
@@ -1,16 +1,15 @@
 import os
 
+
 def git_data():
     return {
-        "gitusername": os.popen("git config user.name").read().strip(),
-        "gitemail": os.popen("git config user.email").read().strip(),
-#        "gitrepo": os.popen("git remote get-url origin").read().strip().rsplit(".",1)[0]
+        b"gitusername": os.popen("git config user.name").read().strip().encode(),
+        b"gitemail": os.popen("git config user.email").read().strip().encode(),
+        #        "gitrepo": os.popen("git remote get-url origin").read().strip().rsplit(".",1)[0]
     }
+
 
 d = git_data()
 scaffer_out.update(d)
 
-scaffer_out["libname"] = os.path.basename(os.getcwd())
-
-
-
+scaffer_out[b"libname"] = os.path.basename(os.getcwd()).encode()
