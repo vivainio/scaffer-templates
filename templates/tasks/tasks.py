12a3d6949c6a0e74c579c6d5fddcb3ef25544dbf
Ville Vainio <ville.vainio@basware.com>
2024-09-10 13:39:40 +0300

    ruff etc

--- templates/tasks/tasks.py
+++ templates/tasks/tasks.py
@@ -1,78 +1,77 @@
-""" Simple, fast and fun task runner, not unlike gulp / grunt (but zero dep)"""
-import sys
-import subprocess
-import shutil
+"""Simple, fast and fun task runner, not unlike gulp / grunt (but zero dep)"""
+
 import os
+import shutil
+import subprocess
+import sys
 import textwrap
 
 PACKAGE = "scf_prj"
 
 
-def do_check(args):
+def do_check(args) -> None:
     """typecheck, lint etc goes here"""
     c("mypy scf_prj")
 
 
-def do_black(args):
-    """do 'black' reformat of all code"""
+def do_black(args) -> None:
+    """Do 'black' reformat of all code"""
     c("py -m black scf_prj")
 
 
-def do_test(args):
+def do_test(args) -> None:
     os.chdir("test")
     c("pytest")
 
 
-def default():
+def default() -> None:
     show_help()
 
 
 # library functions here (or in own module, whatever, I don't care)
 
-
-def run_node_bin(scriptname: str, arg: str):
-    c(rf"node_modules\.bin\{scriptname} {arg}")
+emit = print
 
 
 def c_spawn(cmd, cwd):
-    print(">", cmd)
+    emit(">", cmd)
     subprocess.Popen(cmd, cwd=cwd, shell=True)
 
 
 def copy_files(sources, destinations):
-    """copy each source to each destinatios"""
+    """Copy each source to each destinatios"""
     for src in sources:
         for dest in destinations:
             src = os.path.abspath(src)
             dest = os.path.abspath(dest)
-            print("cp %s -> %s" % (src, dest))
+            emit("cp %s -> %s" % (src, dest))
             if not os.path.isdir(dest):
-                print("File not found", dest)
+                emit("File not found", dest)
                 continue
             shutil.copy(src, dest)
 
 
 def c(cmd):
-    print(">", cmd)
+    emit(">", cmd)
     subprocess.check_call(cmd, shell=True)
 
 
 def c_ignore(cmd):
-    print(">", cmd)
+    emit(">", cmd)
     subprocess.call(cmd, shell=True)
 
 
 def c_dir(cmd, dir):
-    print("%s > %s" % (dir, cmd))
+    emit("%s > %s" % (dir, cmd))
     subprocess.check_call(cmd, cwd=dir, shell=True)
 
 
 # scaffolding starts. Do not edit below
 
 
-def show_help():
+def show_help() -> None:
     g = globals()
-    print(
+    emit(
         "Command not found, try",
         sys.argv[0],
         " | ".join([n[3:] for n in g if n.startswith("do_")]),
@@ -80,18 +79,18 @@ def show_help():
     )
 
 
-def main():
-    """Launcher. Do not modify"""
+def main() -> None:
+    """Launcher. Do not modify."""
     if len(sys.argv) < 2:
         default()
         return
     func = sys.argv[1]
     f = globals().get("do_" + func)
     if sys.argv[-1] == "-h":
-        print(
+        emit(
             textwrap.dedent(f.__doc__).strip()
             if f.__doc__
-            else "No documentation for this command"
+            else "No documentation for this command",
         )
         return
     if not f:
