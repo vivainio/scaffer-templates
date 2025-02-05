5f8a0f0fa1248d69d8f9874e14c17eed00a7551e
Ville Vainio <ville.vainio@basware.com>
2025-02-05 19:02:15 +0200

    allow functions to return argument parser

--- templates/tasks/tasks.py
+++ templates/tasks/tasks.py
@@ -6,6 +6,8 @@ import shlex
 import subprocess
 import sys
 import textwrap
+from argparse import ArgumentParser, Namespace
+import inspect
 
 
 def do_check(_args: list[str]) -> None:
@@ -28,6 +30,14 @@ def do_test(_args: list[str]) -> None:
     c("pytest")
 
 
+def do_complex(_args: Namespace) -> ArgumentParser | None:
+    if _args is None:
+        parser = ArgumentParser(description="Complex fn")
+        parser.add_argument("--foo", type=int, required=True, help="foo help")
+        return parser
+    print("Complex fn called with", _args)
+
+
 def default() -> None:
     show_help()
 
@@ -36,27 +46,55 @@ def default() -> None:
 
 emit = print
 
+
 def c(
     cmd: list[str | Path] | str, check=True, shell=False, cwd: str | Path | None = None
 ) -> None:
-    """ Run a shell command"""
+    """Run a shell command"""
     cmdtext = shlex.join(str(s) for s in cmd) if isinstance(cmd, list) else cmd
     cwd_text = f"{cwd} " if cwd else ""
     cmdtext = f"{cwd_text}> {cmdtext}"
     emit(cmdtext)
     subprocess.run(cmd, check=check, shell=shell, cwd=cwd)
 
+
 # scaffolding starts. Do not edit below
+
+
+def _is_argparse_function(f) -> bool:
+    annotations = list(p.annotation for p in inspect.signature(f).parameters.values())
+    if len(annotations) == 1 and annotations[0] == Namespace:
+        return True
+    return False
+
+
+def _collect_args_from_argparse_function(f) -> ArgumentParser:
+    parser = f(None)
+    if not isinstance(parser, ArgumentParser):
+        raise ValueError(
+            "Function taking argparse.Namespace must return ArgumentParser when called with None"
+        )
+    parser.prog = "python tasks.py " + f.__name__[3:]
+    return parser
 
 
 def show_help() -> None:
     g = globals()
+    funcs = [(n[3:], f) for (n, f) in g.items() if n.startswith("do_")]
+    funcs.sort()
     emit(
-        "Command not found, try",
-        sys.argv[0],
-        " | ".join([n[3:] for n in g if n.startswith("do_")]),
-        "| <command> -h",
+        "Command not found. Try one of these:",
     )
+    for name, func in funcs:
+        nametext = name + ":"
+
+        if _is_argparse_function(func):
+            parser = _collect_args_from_argparse_function(func)
+            help_text = parser.format_help()
+            emit(nametext)
+            emit(textwrap.indent(help_text, " " * 4))
+        else:
+            emit(f"{nametext:<15} {func.__doc__ or ''}")
 
 
 def main() -> None:
@@ -66,6 +104,16 @@ def main() -> None:
         return
     func = sys.argv[1]
     f = globals().get("do_" + func)
+    if f:
+        if _is_argparse_function(f):
+            parser = _collect_args_from_argparse_function(f)
+            args = parser.parse_args(sys.argv[2:])
+            f(args)
+            return
+        else:
+            f(sys.argv[2:])
+            return
+
     if sys.argv[-1] == "-h":
         emit(
             textwrap.dedent(f.__doc__).strip()
@@ -76,7 +124,6 @@ def main() -> None:
     if not f:
         show_help()
         return
-    f(sys.argv[2:])
 
 
 if __name__ == "__main__":
